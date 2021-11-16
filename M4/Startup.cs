e149f5d81f3ae7fac6813debd23b75efdbade1b1
Darsot <modarsot@gmail.com>
2021-11-16 21:08:28 +0200

    Add project files.

---
+++ M4/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(M4.Startup))]
+namespace M4
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
